af975e30cab6a33d4d886678f5a70ba9a40b36e4
JFank612 <36108855+JFank612@users.noreply.github.com>
2020-02-10 18:49:12 -0600

    Add files via upload

---
+++ Array.kt
@@ -0,0 +1,9 @@
+fun main(arg: Array<String>){
+    var taco = arrayOf<String>("shell", "ground beef", "cheese", "lettuce", "sour cream")
+    var i = 0
+
+    while (i < taco.size) {
+        println(taco[i])
+        i++
+    }
+}
